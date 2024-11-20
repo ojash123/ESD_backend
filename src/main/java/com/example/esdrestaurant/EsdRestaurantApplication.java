fa4a697241de8a373520c68649d1095c65aac005
ojash123 <ojashegde1@gmail.com>
2024-11-20 18:14:34 +0530

    First commit

---
+++ src/main/java/com/example/esdrestaurant/EsdRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example.esdrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsdRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsdRestaurantApplication.class, args);
+    }
+
+}
