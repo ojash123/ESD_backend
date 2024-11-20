bec4e4c80df3b8f48b4b8a9b1d17f776d45ace60
ojash123 <ojashegde1@gmail.com>
2024-11-20 21:15:41 +0530

    Customer controller working, spring security disabled

--- src/main/java/com/example/esdrestaurant/controller/CustomerController.java
+++ src/main/java/com/example/esdrestaurant/controller/CustomerController.java
@@ -1,4 +1,28 @@
 package com.example.esdrestaurant.controller;
 
+
+import com.example.esdrestaurant.dto.CustomerRequest;
+import com.example.esdrestaurant.dto.CustomerResponse;
+import com.example.esdrestaurant.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @GetMapping("/{email}")
+    public ResponseEntity<CustomerResponse> getCustomer(@PathVariable("email") String email) {
+        return ResponseEntity.ok(customerService.retrieveCustomer(email));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
 }
